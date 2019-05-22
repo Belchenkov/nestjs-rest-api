c175dd871c6f9d4522da9706bd8b6d7373422114
Aleksey Belchenkov <u608110@gmail.com>
2019-05-22 21:05:39 +0300

    Create Routes

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # NestJSAPI
 
-### 
+### Explore the Nest JS Node.js framework which allows you to build fast and scalable server-side apps and APIs.
---
+++ src/items/items.controller.ts
@@ -0,0 +1,14 @@
+import { Controller, Get, Post, Put, Delete, Body } from '@nestjs/common';
+import { CreateItemDto } from './dto/create-item.dto';
+
+@Controller('items')
+export class ItemsController {
+  @Get()
+  findAll(): string {
+    return 'Get All items';
+  }
+  @Post()
+  create(@Body() createItemDto: CreateItemDto): string {
+    return `Name: ${createItemDto.name} Desc: ${createItemDto.description}`;
+  }
+}
