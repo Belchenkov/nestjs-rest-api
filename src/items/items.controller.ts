87548e6b1f8be55507531bcc4fcea75f969a81f1
Aleksey Belchenkov <u608110@gmail.com>
2019-05-24 13:39:28 +0300

    Get Item

--- src/items/items.controller.ts
+++ src/items/items.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Put, Delete, Body } from '@nestjs/common';
+import { Controller, Get, Post, Put, Delete, Body, Param } from '@nestjs/common';
 import { CreateItemDto } from './dto/create-item.dto';
 
 @Controller('items')
@@ -6,6 +6,10 @@ export class ItemsController {
   @Get()
   findAll(): string {
     return 'Get All items';
+  }
+  @Get(':id')
+  findOne(@Param('id') id) {
+    return `Item ${id}`;
   }
   @Post()
   create(@Body() createItemDto: CreateItemDto): string {
