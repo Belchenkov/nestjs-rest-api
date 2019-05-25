09d58153f78ed050eacec2e8e4cc9909c0d7c621
Aleksey Belchenkov <u608110@gmail.com>
2019-05-25 13:26:05 +0300

    Get Items from Mongo

--- src/items/items.service.ts
+++ src/items/items.service.ts
@@ -1,33 +1,15 @@
 import { Injectable } from '@nestjs/common';
 import { Item } from './interfaces/item.interface';
+import { Model } from 'mongoose';
+import { InjectModel } from '@nestjs/mongoose';
 
 @Injectable()
 export class ItemsService {
-  private readonly items: Item[] = [
-    {
-      id: '1',
-      name: 'Item One',
-      qty: 100,
-      description: 'This is item one',
-    },
-    {
-      id: '2',
-      name: 'Item Two',
-      qty: 120,
-      description: 'This is item two',
-    },
-    {
-      id: '3',
-      name: 'Item Three',
-      qty: 200,
-      description: 'This is item three',
-    },
-  ];
-
-  findAll(): Item[] {
-    return this.items;
+  constructor(@InjectModel('Item') private readonly itemModel: Model<Item>) {}
+  async findAll(): Promise<Item[]> {
+    return await this.itemModel.find();
   }
-  findOne(id: string): Item {
-    return this.items.find(item => item.id === id);
+  async findOne(id: string): Promise<Item> {
+    return await this.itemModel.findOne({ _id: id });
   }
 }
